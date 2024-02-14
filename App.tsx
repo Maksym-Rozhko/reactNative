bf8e541a7ec9deecd545d3052880374dd0c0ca9b
Maksym <maksymRozhko7@gmail.com>
2024-02-15 01:37:58 +0200

    add custom hook useKeyboard

--- App.tsx
+++ App.tsx
@@ -2,9 +2,10 @@ import 'react-native-gesture-handler';
 
 // import { Navigation } from '@/navigation';
 import React, { useCallback } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
 
 import { useBackHandler } from '@/hooks/useBackHandler';
+import { useKeyboard } from '@/hooks/useKeyboard';
 import { useOrientation } from '@/hooks/useOrientation';
 // import { SafeAreaProvider } from 'react-native-safe-area-context';
 // import { HomeScreen } from '@/screens/Home/HomeScreen';
@@ -13,9 +14,10 @@ import { useOrientation } from '@/hooks/useOrientation';
 
 export default function App() {
   const orientation = useOrientation();
+  const { keyboardShown, keyboardHeight, keyboardWidth } = useKeyboard();
 
   const handleBack = useCallback(() => {
-    console.warn('Нажата кнопка назад');
+    console.warn('Натиснуто кнопку Назад');
     return true;
   }, []);
 
@@ -23,7 +25,19 @@ export default function App() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>{`Orientation: ${orientation}`}</Text>
+      <TextInput
+        style={{ height: 40, width: 200, borderColor: 'gray', borderWidth: 1, marginTop: 20 }}
+        placeholder="Введіть текст"
+      />
+      <Text>Клавіатурa {keyboardShown ? 'відкрита' : 'закрита'}</Text>
+      {keyboardShown && (
+        <View>
+          <Text>{`Висота клавіатури: ${keyboardHeight}`}</Text>
+          <Text>{`Ширина клавіатури: ${keyboardWidth}`}</Text>
+        </View>
+      )}
     </View>
+
     // <Navigation />
     // <SafeAreaProvider>
     //   <HomeScreen />
