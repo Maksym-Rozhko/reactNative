b4d048a4fbaf30c9103d2dc31fc46e7d562a09df
Maksym <maksymRozhko7@gmail.com>
2024-02-25 23:15:13 +0200

    basket total

--- src/screens/Basket/Basket.tsx
+++ src/screens/Basket/Basket.tsx
@@ -1,22 +1,52 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import styles from './BasketStyles';
+import { CustomPressable } from '../../components/CustomPressable/CustomPressable';
 import { Item } from '../../components/Item/Item';
 import { RootState } from '../../store';
+import { clearCart } from '../../store/basket/basketSlice';
 
 const BasketScreen = () => {
   const basketItems = useSelector((state: RootState) => state.basket.cartItems);
+  const dispatch = useDispatch();
+
+  const totalAmount = basketItems
+    .reduce((total, item) => {
+      return total + parseFloat(item.newPrice) * item.quantity;
+    }, 0)
+    .toFixed(2);
+
+  const handleClearBasket = () => {
+    dispatch(clearCart());
+  };
 
   return (
     <View style={styles.container}>
       {basketItems.length === 0 ? (
         <Text style={styles.screenTitle}>Ваша корзина пуста</Text>
       ) : (
-        <View>
-          <FlatList data={basketItems} renderItem={({ item }) => <Item itemData={item} isInBasket />} />
+        <View style={styles.flatlistParent}>
+          <FlatList
+            data={basketItems}
+            renderItem={({ item }) => <Item itemData={item} isInBasket />}
+            nestedScrollEnabled
+            maxToRenderPerBatch={3}
+            initialNumToRender={3}
+          />
+          <View style={styles.basketTotal}>
+            <Text style={styles.basketTotalPriceText}>
+              Общая сумма заказа: <Text style={styles.basketTotalPrice}>${totalAmount}</Text>
+            </Text>
+            <CustomPressable onPress={handleClearBasket} style={styles.basketClearBtn}>
+              <Text style={styles.basketClearBtnText}>Очистити кошик</Text>
+            </CustomPressable>
+            <CustomPressable onPress={handleClearBasket} style={styles.basketToCheckout}>
+              <Text style={styles.basketToCheckoutText}>Оформити замовлення</Text>
+            </CustomPressable>
+          </View>
         </View>
       )}
     </View>
--- src/screens/Basket/BasketStyles.ts
+++ src/screens/Basket/BasketStyles.ts
@@ -10,6 +10,51 @@ const styles = StyleSheet.create({
     marginTop: 8,
     fontWeight: 'bold',
   },
+  flatlistParent: {
+    maxHeight: 700,
+  },
+  basketTotal: {
+    marginTop: 20,
+    padding: 20,
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    gap: 10,
+    backgroundColor: 'rgba(0,0,0, .7)',
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: 'green',
+  },
+  basketTotalPriceText: {
+    fontSize: 16,
+    color: '#fff',
+    marginBottom: 15,
+  },
+  basketTotalPrice: {
+    fontSize: 22,
+    color: '#fff',
+    fontWeight: 'bold',
+    marginLeft: 10,
+  },
+  basketClearBtn: {
+    padding: 10,
+    backgroundColor: '#BC1B26',
+    borderRadius: 10,
+  },
+  basketClearBtnText: {
+    color: '#fff',
+    fontSize: 15,
+  },
+  basketToCheckout: {
+    padding: 10,
+    backgroundColor: 'rgb(94, 150, 26)',
+    borderRadius: 10,
+  },
+  basketToCheckoutText: {
+    color: '#fff',
+    fontSize: 15,
+  },
 });
 
 export default styles;
